d2596af9ca6f48e8cfedb020d019c2cb713e4857
Lana <lanahappylot89@gmail.com>
2022-05-28 16:03:25 +0300

    Svetlana Kharlanekova lesson1 done (I think :) )

--- src/svetlana_kharlanenkova/lesson_1/main.dart
+++ src/svetlana_kharlanenkova/lesson_1/main.dart
@@ -1,10 +1,33 @@
-void main(List<String> args) {
-  double apelsin(q, w) {
-    return q/w;}
- var q = 21;
- var w = 8;
+double apelsin(q, w) {
+  return q/w;
+}
 
- var c = apelsin(q, w);
+int apelsin2(n) {
+  final a = n%10;
+  final b = n~/10;
+  return (a+b);
+}
+
+int apelsin3(n) {
+  return n.round();
+}
+
+int apelsin4(n) {
+  final a = n~/100;
+  final b = (n/10%10).round();
+  final c = n%10;
+  return (a+b+c);
+}
+
+void main(List<String> args) {
+
+ const q = 21;
+ const w = 8;
+
+ final c = apelsin(q, w);
 
  print(c);
+ print(apelsin2(52));
+ print(apelsin3(7.5));
+ print(apelsin4(123));
 }
