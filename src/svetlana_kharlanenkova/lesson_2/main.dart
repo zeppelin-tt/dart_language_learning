bfa9e1a4a178e197da34005e0105a559d5efb6db
Lana <lanahappylot89@gmail.com>
2022-05-29 18:49:59 +0300

    Svetlana Kharlanenkova lesson2 - done :) Sorry, just added first part of the task

--- src/svetlana_kharlanenkova/lesson_2/main.dart
+++ src/svetlana_kharlanenkova/lesson_2/main.dart
@@ -1,8 +1,90 @@
+class Dinosaur {
+  String name;
+  String color;
+  String type;
+
+  Dinosaur(this.name, this.color, this.type);
+}
+
+class Theropoda extends Dinosaur {
+  String teeth = 'sharp';
+  Theropoda(
+    name,
+    color,
+    type,
+  ) : super(
+          name,
+          color,
+          type,
+        );
+}
+
+class Coelurosauria extends Theropoda {
+  bool hasFeathers;
+  Coelurosauria(
+    name,
+    color,
+    type,
+    this.hasFeathers,
+  ) : super(
+          name,
+          color,
+          type,
+        );
+}
+
+class Maniraptora extends Coelurosauria {
+  bool hasBeak;
+  Maniraptora(
+    name,
+    color,
+    type,
+    hasFeathers,
+    this.hasBeak,
+  ) : super(
+          name,
+          color,
+          type,
+          hasFeathers = true,
+        );
+}
+
+class Dromaeosauridae extends Maniraptora {
+  bool hasBigClaw;
+  Dromaeosauridae(
+    name,
+    color,
+    type,
+    hasFeathers,
+    hasBeak,
+    this.hasBigClaw,
+  ) : super(
+          name,
+          color,
+          type,
+          hasFeathers = true,
+          hasBeak = false,
+        );
+}
+
 void main() {
   final person = Person('Camel Brown', 100);
   final user = User('Llama White', 70);
-  final student = Student('German Alpaca bananahammock', 55, DateTime(2006, 8, 1));
+  final student =
+      Student('German Alpaca bananahammock', 55, DateTime(2006, 8, 1));
   print(student);
+
+  final dinosaur = Dinosaur('dino', 'green', 'unknown');
+  final tRex = Theropoda('evil', 'brown', 'carnivore');
+  final therizinosaurus = Coelurosauria('kind', 'light brown', 'herbivore', true);
+  final oviraptor = Maniraptora('ovi', 'pink', 'carnivore', true, true);
+  final deinonychus = Dromaeosauridae('extra evil', 'grey', 'carnivore', true, true, true);
+
+  print(dinosaur);
+  print(tRex);
+  print(therizinosaurus);
+  print(oviraptor);
+  print(deinonychus);
 }
 
 class Person {
