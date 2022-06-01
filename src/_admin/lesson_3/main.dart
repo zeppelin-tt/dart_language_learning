87a04f7b198fb8e23352e55169b447134e50da0f
schennikov maksim <zeppelininyourmind@gmail.com>
2022-06-02 01:25:56 +0300

    homeworks

---
+++ src/_admin/lesson_3/main.dart
@@ -0,0 +1,83 @@
+void main() {
+  final animal1 = Animal(45, name: 'Dog');
+
+  final cat1 = Cat(size: 10, name: 'Peter');
+  final cat2 = Cat(size: 5, name: 'Vasya');
+  final dog = Dog(size: 15, name: 'Sharik');
+  final sergey = Sergey<Cat, Dog, String>();
+  sergey.handleSomething(object1: cat1);
+
+}
+
+class Sergey<T, B, C> {
+  void handleSomething({T? object1, B? object2, C? object3}) {
+
+  }
+}
+
+abstract class Jumpable {
+  void jump();
+}
+
+mixin NightVision {
+  void switchOnNightVision() {
+   print('night vision switched on');
+  }
+}
+
+
+
+class Dog extends Animal with NightVision implements Jumpable {
+
+  Dog({
+    required String name,
+    required int size,
+  }) : super(size, name: name);
+
+  @override
+  void showVoice() {
+    print('$name sad Woof');
+  }
+
+  @override
+  void jump() {}
+}
+
+class Cat extends Animal {
+
+  Cat({
+    required String name,
+    required int size,
+  }) : super(size, name: name);
+
+  @override
+  void showVoice() {
+    print('$name sad Mey');
+  }
+}
+
+class Animal {
+  final String? name;
+  final int size;
+
+  const Animal(
+    this.size, {
+    this.name,
+  });
+
+  void showVoice() {
+    print('$name sad');
+  }
+}
+
+class Picture {}
+
+class Size {
+  final double height;
+  final double width;
+
+  const Size({
+    required this.height,
+    required this.width,
+  });
+}
