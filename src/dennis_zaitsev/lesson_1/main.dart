de106408622b9a0458c880ef97736b4c9282478f
Dennis Zaitsev <jeroldminor@bk.ru>
2022-06-05 12:56:41 +0400

    хомворк для леccн ван: комплит)))

--- src/dennis_zaitsev/lesson_1/main.dart
+++ src/dennis_zaitsev/lesson_1/main.dart
@@ -7,6 +7,10 @@ void main() {
   rounding(27.21);
   //job 4
   summ2(108);
+  //extra job 1
+  rectangleArea(4, 9);
+  //extra job 2
+  perimeterSquare(7);
 }
 //job 1
 void division(int q, int w){
@@ -24,13 +28,23 @@ void summ (int n){
 //job 3
 void rounding(double n) {
   int num = n.round();
-  print('Ближайшее целое число от $n составит $num');
+  print('Ближайшее целое число от $n составляет $num');
 }
-// job 2
+// job 4
 void summ2 (int n) {
   int a = n ~/ 100;
   int b = n - 108;
   int c = n % 100;
   int d = a + b + c;
   print('Сумма чисел трёхзначного числа $n равняется $d');
+}
+//extra job 1
+void rectangleArea(int a, int b) {
+  int sum = a * b;
+  print('Площадь прямоугольника со сторонами $a и $b равняется $sum');
+}
+//extra job 2
+void perimeterSquare(int n) {
+  int res = n * 4;
+  print('Периметр квадрата со сторонами $n равен $res');
 }
