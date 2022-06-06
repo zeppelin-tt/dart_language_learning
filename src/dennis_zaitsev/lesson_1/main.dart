1ccf37b864c0dfd8107ca26f709024abd138aee9
Dennis Zaitsev <jeroldminor@bk.ru>
2022-06-06 21:14:46 +0400

    хомворк для леccн ван: комплит)))

--- src/dennis_zaitsev/lesson_1/main.dart
+++ src/dennis_zaitsev/lesson_1/main.dart
@@ -2,11 +2,11 @@ void main() {
   //job 1
   division(21, 8);
   //job 2
-  summ(43);
+  summ(99);
   //job 3
-  rounding(27.21);
+  rounding(55.44);
   //job 4
-  summ2(108);
+  summ2(123);
   //extra job 1
   rectangleArea(4, 9);
   //extra job 2
@@ -22,8 +22,8 @@ void division(int q, int w){
 void summ(int n){
   final firstNum = n ~/ 10;
   final secondNum = n % 10;
-  final res = firstNum + secondNum;
-  print('Сумма чисел двухначного числа $n равняется $res');
+  final result = firstNum + secondNum;
+  print('Сумма чисел двухначного числа $n равняется $result');
 }
 //job 3
 void rounding(double n) {
@@ -34,17 +34,17 @@ void rounding(double n) {
 void summ2(int n) {
   final firstNum = n ~/ 100;
   final secondNum = n % 100 ~/ 10;
-  final thirdNum  = n % 100;
-  final res = firstNum + secondNum + thirdNum;
-  print('Сумма чисел трёхзначного числа $n равняется $res');
+  final thirdNum  = n % 10;
+  final result = firstNum + secondNum + thirdNum;
+  print('Сумма чисел трёхзначного числа $n равняется $result');
 }
 //extra job 1
 void rectangleArea(int a, int b) {
-  int sum = a * b;
-  print('Площадь прямоугольника со сторонами $a и $b равняется $sum');
+  int recResult = a * b;
+  print('Площадь прямоугольника со сторонами $a и $b равняется $recResult');
 }
 //extra job 2
 void perimeterSquare(int n) {
-  int res = n * 4;
-  print('Периметр квадрата со сторонами $n равен $res');
+  int perResult = n * 4;
+  print('Периметр квадрата со сторонами $n равен $perResult');
 }
