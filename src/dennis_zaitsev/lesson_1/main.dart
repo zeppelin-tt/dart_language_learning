2aa4a331abe7480b2e822a4d5188c1fcde1d5100
Dennis Zaitsev <jeroldminor@bk.ru>
2022-06-08 00:40:42 +0400

    хомворк для леccн ван: комплит)))

--- src/dennis_zaitsev/lesson_1/main.dart
+++ src/dennis_zaitsev/lesson_1/main.dart
@@ -2,11 +2,11 @@ void main() {
   //job 1
   division(21, 8);
   //job 2
-  summ(99);
+  print(summ1 (99));
   //job 3
   rounding(55.44);
   //job 4
-  summ2(123);
+  print(summ2 (123));
   //extra job 1
   rectangleArea(4, 9);
   //extra job 2
@@ -19,11 +19,10 @@ void division(int q, int w){
   print('Результат деления: $res1, остаток: $res2');
 }
 // job 2
-void summ(int n){
+int summ1(int n){
   final firstNum = n ~/ 10;
   final secondNum = n % 10;
-  final result = firstNum + secondNum;
-  print('Сумма чисел двухначного числа $n равняется $result');
+  return firstNum + secondNum;
 }
 //job 3
 void rounding(double n) {
@@ -31,12 +30,11 @@ void rounding(double n) {
   print('Ближайшее целое число от $n составляет $num');
 }
 // job 4
-void summ2(int n) {
+int summ2(int n) {
   final firstNum = n ~/ 100;
   final secondNum = n % 100 ~/ 10;
   final thirdNum  = n % 10;
-  final result = firstNum + secondNum + thirdNum;
-  print('Сумма чисел трёхзначного числа $n равняется $result');
+  return firstNum + secondNum + thirdNum;
 }
 //extra job 1
 void rectangleArea(int a, int b) {
