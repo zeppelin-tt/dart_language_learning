e05ffe32b2479d547a72e2b0d47e1f75c92c77cb
Heliotropoff <gregoryevgrafov@gmail.com>
2022-06-14 21:27:44 +0300

    Lessons 1 & 2 update

--- src/gregory_evgrafov/lesson_1/HW1.dart
+++ src/gregory_evgrafov/lesson_1/HW1.dart
@@ -1,54 +1,59 @@
-//task1
-int q = 21;
-int w = 8;
-num division(n1, n2) {
-  double result = q / w;
-  print(result);
-  return result;
-}
-
-//tak2
-int n = 21;
-
-num figure_sum(arg) {
-  String figures = n.toString();
-  String fig1 = figures[0];
-  String fig2 = figures[1];
-  int n1 = int.parse(fig1);
-  int n2 = int.parse(fig2);
-  int result = n1 + n2;
-  print(result);
-  return result;
-}
-
-//task3
-
-double x = 3.14;
-int rounder(decimal) {
-  int result = decimal.round();
-  print(result);
-  return result;
-}
-
-//task4
-// без циклом не понял как можно проитерировать стринг
-int numero = 123;
-num figure3_sum(arg) {
-  String figures = arg.toString();
-  String fig1 = figures[0];
-  String fig2 = figures[1];
-  String fig3 = figures[2];
-  int n1 = int.parse(fig1);
-  int n2 = int.parse(fig2);
-  int n3 = int.parse(fig3);
-  int result = n1 + n2 + n3;
-  print(result);
-  return result;
-}
-
 void main() {
+  // 1. В переменных q и w хранятся два натуральных числа. Создайте функцию,
+  // выводящую на экран результат деления q на w с остатком.
+  final int q = 21;
+  final int w = 8;
+  num division(n1, n2) {
+    double result = q / w;
+    print(result);
+    return result;
+  }
+
+  // 2. В переменной n хранится натуральное двузначное число. Создайте функцию,
+  // вычисляющую и выводящую на экран сумму цифр числа n.
+ final int n = 21;
+
+  int figure_sum(arg) {
+    String figures = n.toString();
+    String fig1 = figures[0];
+    String fig2 = figures[1];
+    int n1 = int.parse(fig1);
+    int n2 = int.parse(fig2);
+    int result = n1 + n2;
+    print(result);
+    return result;
+  }
+
+  // 3. В переменной n хранится вещественное число с ненулевой дробной частью. Создайте функцию,
+  // округляющую число n до ближайшего целого и выводящую результат на экран.
+
+  final double x = 3.14;
+  int rounder(decimal) {
+    int result = decimal.round();
+    print(result);
+    return result;
+  }
+
+  // 4. В переменной n хранится натуральное трёхзначное число. Создайте функцию,
+  // вычисляющую и выводящую на экран сумму цифр числа n.
+
+  // без циклом не понял как можно проитерировать стринг
+  int numero = 123;
+  int figure3Sum(arg) {
+    String figures = arg.toString();
+    String fig1 = figures[0];
+    String fig2 = figures[1];
+    String fig3 = figures[2];
+    int n1 = int.parse(fig1);
+    int n2 = int.parse(fig2);
+    int n3 = int.parse(fig3);
+    int result = n1 + n2 + n3;
+    print(result);
+    return result;
+  }
+
   division(q, w);
   figure_sum(n);
   rounder(x);
-  figure3_sum(numero);
+  figure3Sum(numero);
 }
--- src/gregory_evgrafov/lesson_2/HW2.dart
+++ src/gregory_evgrafov/lesson_2/HW2.dart
@@ -1,48 +1,64 @@
+void main() {
+  koruskaEater ivan = koruskaEater();
+  print(ivan.live);
+  ivan.speak();
+  ivan.question();
+
+  Person mysterious_guest = Person();
+  mysterious_guest.move();
+  mysterious_guest.talk();
+  print(mysterious_guest.age);
+
+  Person jack = Person('Jack', 35);
+  jack.move();
+  jack.talk();
+  print(jack.age);
+
+  Student student1 = Student('Elvis', 'Jackson', 2020);
+  student1.FIO();
+}
+
 // Задачи
 // 1. Создайте цепочку реальных объектов наследования, длиною 5 объектов. У каждого объекта должны быть поля и каждый child должен добавлять новые поля.
 //
-class Human{
+class Human {
   String live = 'carbon based';
 
-  void breathe(){
-    print("Breathes");
+  void breathe() {
+    print('Breathes');
   }
 
-  void speak(){
-    print("Speaks");
+  void speak() {
+    print('Speaks');
   }
 }
 
 class Man extends Human {
-  static const  String voice = "deep voice";
+  static const String voice = 'deep voice';
   @override
-  void speak(){
-    print("Speaks in a $voice");
+  void speak() {
+    print('Speaks in a $voice');
   }
 }
 
-class Russian_man extends Man{
-
-  String language = "Russian";
+class RussianMan extends Man {
+  String language = 'Russian';
   @override
-  void speak(){
+  void speak() {
     super.speak();
-    print("Speaks $language");
+    print('Speaks $language');
   }
 }
 
-class Petersburger extends Russian_man{
-
-  String citizenship = "Lives in Saint-Petersburg";
+class Petersburger extends RussianMan {
+  String citizenship = 'Lives in Saint-Petersburg';
 }
 
-class KoruskaEater extends Petersburger{
-
-  void question(){
-    print("How much is the korushka?\n Nana na na na!!!\n Na\nNaNa\nNa");
+class koruskaEater extends Petersburger {
+  void question() {
+    print('How much is the korushka?\n Nana na na na!!!\n Na\nNaNa\nNa');
   }
 }
-
 
 // 2. Создать класс Person, который содержит:
 // переменные fullName, age;
@@ -51,50 +67,23 @@ class KoruskaEater extends Petersburger{
 // Создайте два объекта этого класса. Один объект инициализируется конструктором Person(), другой - Person(fullName, age).
 //
 
-
-class Person{
-
-  String fullName=" ";
-  int age=0;
+class Person {
+  String fullName = " ";
+  int age = 0;
 //квадратные скобки делают указание атрибутов необязательным необязательными
   Person([this.fullName = 'Someone', this.age = 0]);
 
-  void move(){
-    print("$fullName walks");
+  void move() {
+    print('$fullName walks');
   }
 
-  void talk(){
-    print ("$fullName talks");
+  void talk() {
+    print('$fullName talks');
   }
 }
+
 // 3. Реализуйте класс Student (Студент), который будет наследоваться от класса User.
 // Класс должен иметь следующие свойства:
-class User{
-  String name = "";
-  String second_name = "";
-  User(this.name, this.second_name);
-
-  void FIO(){
-    print("$name");
-    print("$second_name");
-  }
-}
-
-class Student extends User{
-  int yearOfAdmission = 0;
-  int currentCourse = 0;
-
-  Student(String name, String second_name, int yearOfAdmission): super (name, second_name ){
-    this.yearOfAdmission = yearOfAdmission;
-    this.currentCourse = DateTime.now().year - yearOfAdmission;
-  }
-  @override
-  void FIO(){
-    super.FIO();
-    print("Год поступления: $yearOfAdmission");
-    print("Курс: $currentCourse");
-  }
-}
 
 // yearOfAdmission:DateTime (год поступления в вуз): инициализируется в конструкторе
 // currentCourse:int (текущий курс): рассчитывается след. образом - из текущего года вычесть год поступления.
@@ -105,27 +94,30 @@ class Student extends User{
 // имя и фамилию студента - используя родительскую реализацию toString
 // год поступления
 // текущий курс
+class User {
+  final String name;
+  final String second_name;
+  User(this.name, this.second_name);
 
-void main(){
-
-  KoruskaEater ivan = KoruskaEater();
-  print(ivan.live);
-  ivan.speak();
-  ivan.question();
-
-  Person mysterious_guest = Person();
-  mysterious_guest.move();
-  mysterious_guest.talk();
-  print(mysterious_guest.age);
-
-  Person jack = Person("Jack", 35);
-  jack.move();
-  jack.talk();
-  print(jack.age);
-
-  Student student1 = Student("Elvis", "Jackson", 2020);
-  student1.FIO();
+  void FIO() {
+    print('$name');
+    print('$second_name');
+  }
 }
 
+class Student extends User {
+  int yearOfAdmission = 0;
+  int currentCourse = 0;
 
+  Student(String name, String second_name, int yearOfAdmission) : super(name, second_name) {
+    this.yearOfAdmission = yearOfAdmission;
+    this.currentCourse = DateTime.now().year - yearOfAdmission;
+  }
+  @override
+  void FIO() {
+    super.FIO();
+    print('Год поступления: $yearOfAdmission');
+    print('Курс: $currentCourse');
+  }
+}
 
