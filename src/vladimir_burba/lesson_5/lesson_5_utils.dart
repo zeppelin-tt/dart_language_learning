50cde59673fbcc73f013feb7a42c871d1972a7cb
vladimir_burba <dogmat-igwt@mail.ru>
2022-07-07 23:42:30 +0300

    vladimir burba lesson 5

---
+++ src/vladimir_burba/lesson_5/lesson_5_utils.dart
@@ -0,0 +1,54 @@
+import 'dart:math';
+
+List<int> generateRandomIntList(int length, int maxValue) {
+  final Random rnd = Random();
+  final List<int> result = [];
+  for (int i = 0; i < length; i++) {
+    result.add(rnd.nextInt(maxValue));
+  }
+  return result;
+}
+
+List<int> deleteMultiplesFromListMass(List<int> listInt, List<int> multipliers) {
+  List<int> result = listInt.toList();
+  for (int i = 0; i < multipliers.length; i++) {
+    result = deleteMultiplesFromList(result, multipliers[i]);
+  }
+  return result;
+}
+
+List<int> deleteMultiplesFromList(List<int> listInt, int multiplier) {
+  return listInt.toList()..removeWhere((element) => element % multiplier == 0);
+}
+
+num evenCompareFunc(int item) {
+  return item.isEven ? 1 / (-1 * item) : item;
+}
+
+Comparator<int> evenComparator = (a, b) => evenCompareFunc(a).compareTo(evenCompareFunc(b));
+
+List<int> sortList(List<int> listInt, [Comparator<int>? compare]) {
+  return compare == null ? (listInt.toList()..sort()) : (listInt.toList()..sort(compare));
+}
+
+List deleteListBorders(List list) {
+  return list.length >= 2
+      ? (list.toList()
+        ..removeAt(0)
+        ..removeLast())
+      : [];
+}
+
+List<num> addIncElements(List<num> listInt, [int increment = 1]) {
+  return listInt.toList().expand((element) => addIncElement(element, increment)).toList();
+}
+
+Iterable<num> addIncElement(num element, [int increment = 1]) sync* {
+  yield element;
+  yield element + increment;
+}
+
+num? getFirstElementMoreThen(List<num> list, num that) {
+  final result = list.firstWhere((element) => element > that, orElse: () => -1);
+  return result == -1 ? null : result;
+}
---
+++ src/vladimir_burba/list_utils.dart
@@ -0,0 +1,49 @@
+import 'dart:math';
+
+extension Comparison on List {
+  bool containsAny(List searchList) {
+    return any((element) => searchList.contains(element));
+  }
+}
+
+num getElementsSum(List<num> list) {
+  return list.toList().reduce((value, element) => value + element);
+}
+
+List shuffleElements(List listInt, [Random? rnd]) {
+  return listInt.toList()..shuffle(rnd);
+}
+
+List<num> incListElements(List<num> listInt, [int increment = 1]) {
+  final List<num> result = listInt.toList();
+  for (var i = 0; i < result.length; i++) {
+    result[i] += increment;
+  }
+  return result;
+}
+
+List<String> setPrefix(List list, String prefix) {
+  final List<String> result = [];
+  for (var i = 0; i < list.length; i++) {
+    result.add('$prefix ${list[i]}');
+  }
+  return result;
+}
+
+List<int> parseIntList(List<String> list) {
+  final List<int> result = [];
+  for (var i = 0; i < list.length; i++) {
+    result.add(int.parse(list[i].replaceAll(RegExp(r'\D'), '')));
+  }
+  return result;
+}
+
+List propagateList(List list, int count) {
+  return list.toList().expand((element) => propagateElement(element, count)).toList();
+}
+
+Iterable propagateElement(Object? element, int count) sync* {
+  for (int i = 0; i < count; i++) {
+    yield element;
+  }
+}
