6de5a3090a7ac75046e129f8daac19ba8b56e8f4
AnnaBurba <79003819+AnnaBurba@users.noreply.github.com>
2022-08-20 12:55:24 +0300

    Delete Task_3.dart

--- src/burba_anya/1/Task_3.dart
+++
@@ -1,9 +0,0 @@
-//Домашнее задание 1.3
-import 'dart:math';
-
-void main() {
-  var q = Random().nextDouble()*10;
-  print('Рандомное вещественное число с ненулевой дробной частью - $q');
-  double w = q.roundToDouble();
-  print('Полученное округлённое число - $w');
-}
