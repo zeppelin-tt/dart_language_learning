fda7f00467c1faa1d363b057b0fd5704cb0eca6e
Trachukv <vladyslavtrachuk@gmail.com>
2022-08-24 00:01:49 +0200

    new pull request (lesson 2)

--- src/vladyslav_trachuk/lesson_2/less 2.dart
+++ src/vladyslav_trachuk/lesson_2/less 2.dart
@@ -1,22 +1,18 @@
 // Задачи
 // 1. Создайте цепочку реальных объектов наследования, длиною 5 объектов.
 //У каждого объекта должны быть поля и каждый child должен добавлять новые поля.
-
 // 2. Создать класс Person, который содержит:
 // переменные fullName, age;
 // методы move() и talk(), в которых просто вывести на консоль сообщение -"Такой-то  Person говорит".
 // Добавьте конструктор Person(fullName, age).
 // Создайте два объект этого класса. Объект инициализируется конструктором Person(fullName, age).
-
 // 3. Реализуйте класс Student (Студент), который будет наследоваться от класса Person.
 // Класс должен иметь следующие свойства:
-
 // yearOfAdmission:DateTime (год поступления в вуз): инициализируется в конструкторе
 // currentCourse:int (текущий курс): рассчитывается след. образом - из текущего года вычесть год поступления.
 // чтобы получить настоящее время - DateTime.now()
 // чтобы получить годы из DateTime, воспользуйтесь свойством DateTime.year
 // Класс должен иметь метод toString() , с помощью которого можно вывести:
-
 // имя и фамилию студента - используя родительскую реализацию toString
 // год поступления
 // текущий курс
@@ -31,6 +27,7 @@ class Electronics {
 
 class Appliances extends Electronics {
   final String forKitchen;
+
   Appliances(
     String type,
     String brand,
@@ -41,12 +38,12 @@ class Appliances extends Electronics {
 class KitchenElectronics extends Appliances {
   final String model;
 
-  KitchenElectronics(String type, String brand, String forKitchen, this.model)
-      : super(brand, type, forKitchen);
+  KitchenElectronics(String type, String brand, String forKitchen, this.model) : super(brand, type, forKitchen);
 }
 
 class CoffeeMachine extends KitchenElectronics {
   final int size;
+
   CoffeeMachine(
     String type,
     String brand,
@@ -58,6 +55,7 @@ class CoffeeMachine extends KitchenElectronics {
 
 class CapsulCoffeMachine extends CoffeeMachine {
   final int pressure;
+
   CapsulCoffeMachine(
     String type,
     String brand,
@@ -71,6 +69,7 @@ class CapsulCoffeMachine extends CoffeeMachine {
 class Person {
   final String fullName;
   final int age;
+
   Person(this.fullName, this.age);
 
   String get move {
@@ -99,11 +98,13 @@ class Doctor extends Person {
 
 class Student extends Person {
   final DateTime yearOfAdmission;
+
   Student(
     String fullName,
     int age,
     this.yearOfAdmission,
   ) : super(fullName, age);
+
   int get currentCourse {
     return DateTime.now().year - yearOfAdmission.year;
   }
