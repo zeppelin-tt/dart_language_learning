8dde6a5ab16b9703d769616f9d69a8614dd83174
Trachukv <vladyslavtrachuk@gmail.com>
2022-08-24 21:35:36 +0200

    corrected task Vladyslav Trachuk Lesson 4

--- src/vladyslav_trachuk/lesson_4/lesson_4.dart
+++ src/vladyslav_trachuk/lesson_4/lesson_4.dart
@@ -28,23 +28,17 @@ double intToDouble(int num) {
 // В переменной min лежит число от 0 до 59.
 // Определите в какую четверть часа попадает это число (в первую, вторую, третью или четвертую).
 String whatTime(int min) {
-  var str = 'У вас ошибка';
   if (min <= 0 || min >= 61) {
-    return str;
+    return 'Ошибка';
+  } else if (min >= 0 && min <= 15) {
+    return 'Данный промежуток попадает в первую четверть часа';
+  } else if (min >= 16 && min <= 30) {
+    return 'Данный промежуток попадает во вторую четверть часа';
+  } else if (min >= 31 && min <= 45) {
+    return 'Данный промежуток попадает в третью четверть часа';
+  } else {
+    return 'Данный промежуток попадает в четвертую четверть часа';
   }
-  if (min >= 0 && min <= 15) {
-    str = 'Данный промежуток попадает в первую четверть часа';
-  }
-  if (min >= 16 && min <= 30) {
-    str = 'Данный промежуток попадает во вторую четверть часа';
-  }
-  if (min >= 31 && min <= 45) {
-    str = 'Данный промежуток попадает в третью четверть часа';
-  }
-  if (min >= 46 && min <= 59) {
-    str = 'Данный промежуток попадает в четвертую четверть часа';
-  }
-  return str;
 }
 
 //      4. Дни недели;
@@ -63,97 +57,142 @@ String whatTime(int min) {
 // д) создание вспомогательных функций приветствуется
 enum Lang { ru, eng }
 
-String dayOfTheWeek(int checkDay, Lang lang) {
-  switch (lang) {
-    case Lang.ru:
-      switch (checkDay) {
-        case 1:
-          return 'Понедельник';
-        case 2:
-          return 'Вторник';
-        case 3:
-          return 'Среда';
-        case 4:
-          return 'Четверг';
-        case 5:
-          return 'Пятница';
-        case 6:
-          return 'Суббота';
-        case 7:
-          return 'Воскресенье';
-        default:
-          return 'Ошибка';
-      }
-    case Lang.eng:
-      switch (checkDay) {
-        case 1:
-          return 'Monday';
-        case 2:
-          return 'Tuesday';
-        case 3:
-          return 'Wednesday';
-        case 4:
-          return 'Thursday';
-        case 5:
-          return 'Friday';
-        case 6:
-          return 'Saturday';
-        case 7:
-          return 'Sunday';
-        default:
-          return 'Error';
-      }
-  }
-}
-
 String timeOfDay(int time, Lang lang) {
   switch (lang) {
     case Lang.ru:
-      if (0 < time && time <= 8) {
-        return 'Сейчас ночь';
-      } else if (8 < time && time <= 12) {
-        return 'Сейчас утро';
-      } else if (12 < time && time <= 20) {
-        return 'Сейчас день';
-      } else if (20 < time && time <= 23) {
-        return 'Сейчас вечер';
+      if (time < 0 || time > 25) {
+        return 'Задание 4 Ошибка - не верно указано время!';
+      } else if (time > 0 && time <= 8) {
+        return 'Задание 4 Сейчас ночь';
+      } else if (time > 8 && time <= 12) {
+        return 'Задание 4 Сейчас утро';
+      } else if (time > 12 && time <= 20) {
+        return 'Задание 4 Сейчас день';
       } else {
-        return 'Ошибка - не верно указано время!';
+        return 'Задание 4 Сейчас вечер';
       }
     case Lang.eng:
-      if (0 < time && time <= 8) {
+      if (time < 0 || time > 25) {
+        return 'Error - time is not correct!';
+      } else if (time > 0 && time <= 8) {
         return "It's night now";
-      } else if (8 < time && time <= 12) {
+      } else if (time > 8 && time <= 12) {
         return "It's morning now";
-      } else if (12 < time && time <= 20) {
+      } else if (time > 12 && time <= 20) {
         return "It's day now";
-      } else if (20 < time && time <= 23) {
-        return "It's evening now";
       } else {
         return 'Error - time is not correct!';
       }
   }
 }
 
-typedef MainCallBack = void Function(String callBackForInfo);
+dynamic giveMeToday(
+  int day,
+  int hour,
+  Lang lang,
+) {
+  String giveMeHour(int checkHour) {
+    switch (lang) {
+      case Lang.eng:
+        if (checkHour > 0 && checkHour < 24) {
+          if (checkHour < 8) {
+            return 'Night';
+          } else if (checkHour < 12) {
+            return 'Morning';
+          } else if (checkHour < 20) {
+            return 'Day';
+          } else {
+            return 'Evening';
+          }
+        } else {
+          return 'Error!';
+        }
+      case Lang.ru:
+        if (checkHour > 0 && checkHour <= 24) {
+          if (checkHour <= 8) {
+            return 'Ночь';
+          } else if (checkHour <= 12) {
+            return 'Утро';
+          } else if (checkHour <= 20) {
+            return 'День';
+          } else {
+            return 'Вечер';
+          }
+        } else {
+          return 'Ошибка!';
+        }
+    }
+  }
 
-void myCallBackPrinting(String daysInfo) {
-  print(daysInfo);
-}
+  dynamic giveMeDay(int day) {
+    switch (lang) {
+      case Lang.eng:
+        if (day <= 0 && day >= 8) {
+          if (day == 1) {
+            return 'Понедельник';
+          } else if (day == 2) {
+            return 'Вторник';
+          } else if (day == 3) {
+            return 'Среда';
+          } else if (day == 4) {
+            return 'Четверг';
+          } else if (day == 5) {
+            return 'Пятница';
+          } else if (day == 6) {
+            return 'Суббота';
+          } else if (day == 7) {
+            return 'Воскресенье';
+          } else {
+            return 'Ошибка';
+          }
+        }
+        break;
+      case Lang.ru:
+        if (day <= 0 && day >= 8) {
+          if (day == 1) {
+            return 'Monday';
+          } else if (day == 2) {
+            return 'Tuesday';
+          } else if (day == 3) {
+            return 'Wednesday';
+          } else if (day == 4) {
+            return 'Thursday';
+          } else if (day == 5) {
+            return 'Friday';
+          } else if (day == 6) {
+            return 'Saturday';
+          } else if (day == 7) {
+            return 'Sunday';
+          } else {
+            return 'Error!';
+          }
+        }
+    }
+  }
 
-String checkingTheTime(int checkDay, int time, Lang lang, [MainCallBack? onResult]) {
-  final info =
-      ' (КОЛЛБЕК) Сегодня день недели - ${dayOfTheWeek(checkDay, lang)}, а время суток - ${timeOfDay(time, lang)}';
-  onResult?.call(info);
-  return 'Сегодня прекрасный день - ${dayOfTheWeek(checkDay, lang)} не правда ли? Хотя сейчас всего лишь - ${timeOfDay(time, lang)} ';
+  final thisDay = giveMeDay(day);
+  final thisHour = giveMeHour(hour);
+  switch (lang) {
+    case Lang.eng:
+      if (thisHour == 'Error!' || thisDay == 'Error!') {
+        return 'Error!';
+      } else {
+        return 'Today is $thisDay and it`s $thisHour';
+      }
+    case Lang.ru:
+      if (thisHour == 'Ошибка!' || thisDay == 'Ошибка!') {
+        return 'Ошибка!';
+      } else {
+        return 'Сегодня $thisDay и сейчас $thisHour';
+      }
+  }
 }
 
 void main() {
   print(checkNum(10));
-  print(whatTime(59));
-  print(timeOfDay(10, Lang.ru));
-  print(timeOfDay(23, Lang.eng));
-  print(timeOfDay(11, Lang.ru));
-  print(checkingTheTime(5, 18, Lang.ru, myCallBackPrinting));
-  print(checkingTheTime(5, 8, Lang.ru));
+  print(whatTime(24));
+  print(timeOfDay(-1, Lang.ru));
+  print(timeOfDay(12, Lang.eng));
+  print(timeOfDay(28, Lang.ru));
+  print(giveMeToday(3, 21, Lang.ru));
 }
