d8aa8fdeda34ed121f363aa907d959769972e095
agumirovz <agumirovz@gmail.com>
2022-08-26 13:38:13 +0600

    fixed

--- src/askhat_gumirov/lesson_5/main.dart
+++ src/askhat_gumirov/lesson_5/main.dart
@@ -18,15 +18,15 @@ void main() {
   print('Лист с увеличенными на 1 элементами ${listOfInt.map((e) => e + 1)}');
   final List<int> increasedElements = listOfInt.expand((element) => [element, (element + 100)]).toList();
   print('Лист с элементами увеличенными на 100 $increasedElements');
-  int result = increasedElements.firstWhere((element) => element > 100);
-  print('Первый элемент больше 100 $result');
-  result =
+  final int firstLarger100 = increasedElements.firstWhere((element) => element > 100);
+  print('Первый элемент больше 100 $firstLarger100');
+  final indexOfFirstLarger100 =
       increasedElements.indexWhere((element) => element == increasedElements.firstWhere((element) => element > 100));
-  print('Индекс первого элемента больше 100 $result');
-  final int newResult = increasedElements.firstWhere(compare, orElse: () => 0);
+  print('Индекс первого элемента больше 100 $indexOfFirstLarger100');
+  final newResult = increasedElements.firstWhere(compare, orElse: () => 0);
   print(newResult == 0 ? 'Массив не содержит элементов из списка $compareList' : 'Массив содержит элемент $newResult');
-  result = increasedElements.fold(0, (previousValue, element) => previousValue += element);
-  print('Сумма элементов массива = $result');
+  final int sumOfElements = increasedElements.fold(0, (previousValue, element) => previousValue += element);
+  print('Сумма элементов массива = $sumOfElements');
   final List<String> listToString = [];
   listToString.addAll(increasedElements.map((element) => 'Number $element'));
   print(listToString);
@@ -44,12 +44,12 @@ void main() {
   print('Сет');
   print(stringToIntList.toSet());
   print('Мап');
-  Map<int, int> mapOfInt;
-  print(mapOfInt = stringToIntList.asMap());
+  final mapOfInt = stringToIntList.asMap();
+
   print('Золотое сечение');
   final GoldenRationSize goldenRationSize = GoldenRationSize(5);
   print(goldenRationSize);
-  final Map<int,GoldenRationSize> mapOfGoldenRation = mapOfInt.map((key, value) => MapEntry(key, GoldenRationSize(value)));
+  final mapOfGoldenRation = mapOfInt.map((key, value) => MapEntry(key, GoldenRationSize(value)));
   print(mapOfGoldenRation);
 }
 
@@ -71,8 +71,8 @@ bool compare(int value) {
 }
 
 class GoldenRationSize {
-  int smallSide;
-  int bigSide;
+  final int smallSide;
+  final int bigSide;
   GoldenRationSize(this.smallSide): bigSide = (smallSide * 1.61803).round();
 
   @override
