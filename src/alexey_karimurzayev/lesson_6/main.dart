df0ef766ec5898b83888a0627f51de6578c10050
Alexey Karimurzayev <asxtream@gmail.com>
2022-08-27 23:01:08 +0600

    alexey karimurzayev lesson 6: completed

--- src/alexey_karimurzayev/lesson_6/main.dart
+++ src/alexey_karimurzayev/lesson_6/main.dart
@@ -1,10 +1,9 @@
-// 1. Для данного натурального числа n напечатайте число,
-// записанное теми же десятичными цифрами, но в обратном порядке.
-// Задание выполнить через цикл.
-
 import 'dart:math';
 
 void main() {
+  // 1. Для данного натурального числа n напечатайте число,
+  // записанное теми же десятичными цифрами, но в обратном порядке.
+  // Задание выполнить через цикл.
   const int n = 12345678;
   final List<String> stringList = n.toString().split('');
   final List<int> intList = stringList.map(int.parse).toList();
@@ -19,9 +18,7 @@ void main() {
   // Для генерации случайного числа использовать Random() из пакета math.
   // Не следует каждый раз создавать экземляр класса Random()
   // b) Создайте формулы для получения максимального, минимального и среднего значения.
-  // c) Прочитайте про различные виды сортировок.
-  // https://academy.yandex.ru/posts/osnovnye-vidy-sortirovok-i-primery-ikh-realizatsii
-  // d) Реализуйте "сортировку пузырьком" данного листа.
+
   tempString = tempList.map((e) => e.toString()).join();
   newN = int.parse(tempString);
   print(newN);
@@ -51,9 +48,10 @@ void main() {
   averageNumber = (temp / list.length).round();
   print('Среднее значение = $averageNumber');
 
+  // d) Реализуйте "сортировку пузырьком" данного листа.
   List<int> babbleSort(List<int> list) {
-    for (int i = 0; i < list.length - 1; i++) {
-      for (int j = 0; j < list.length - 1 - 1; j++) {
+    for (int i = 0; i <= list.length - 1; i++) {
+      for (int j = 0; j < list.length - i - 1; j++) {
         if (list[j] > list[j + 1]) {
           final int temp = list[j];
           list[j] = list[j + 1];
@@ -65,12 +63,45 @@ void main() {
   }
 
   final List<int> sortList = babbleSort(list);
-  print(sortList);
+  print('Сортировка пузырьком - $sortList');
 
   // 3. Напишите программу, которая выводит на консоль простые числа в промежутке от [2, 500].
   // Используйте для решения этой задачи оператор "%" (остаток от деления) и циклы.
+
+  final findPrimeList = List.generate(15, (index) => randomList.nextInt(498) + 2);
+  print('Просто набор из 15 чисел от 2 до 500 - $findPrimeList');
+  final List<int> primeNumbers = [];
+
+  for (int n = 0; n <= findPrimeList.length - 1; n++) {
+    bool isPrime = true;
+    final double squareRoot = sqrt(findPrimeList[n]) + 2;
+    int j = 2;
+    while (j <= squareRoot && isPrime) {
+      if (findPrimeList[n] % j == 0) isPrime = false;
+      j++;
+    }
+    if (isPrime) {
+      primeNumbers.add(findPrimeList[n]);
+    }
+  }
+  print(primeNumbers);
+
   // 4. Даны два целых числа A и В. Используюя рекурсию, выведите все числа от A до B включительно,
   // в порядке возрастания, если A < B, или в порядке убывания в противном случае.
+  int count = 0;
+  void numbersAtoB(int a, int b) {
+    if (a > b) {
+      numbersAtoB(a, b + 1);
+      count++;
+      print('$a > $b - увеличиваем b на 1, b = $b, счетчик - $count');
+    } else if (a < b) {
+      numbersAtoB(a + 1, b);
+      count++;
+      print('$a < $b увеличиваем a на 1, а = $a, счетчик - $count');
+    } else {
+      print('$a = $b');
+    }
+  }
 
-
+  numbersAtoB(1, 9);
 }
