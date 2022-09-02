b58b2579ed6b7ad578524cf1d796483fd4b12ff4
Trachukv <vladyslavtrachuk@gmail.com>
2022-09-03 00:31:19 +0200

    Vladyslav Trachuk Lesson 8 (corrected)

--- src/vladyslav_trachuk/lesson_8/lesson_8.dart
+++ src/vladyslav_trachuk/lesson_8/lesson_8.dart
@@ -1,23 +1,13 @@
-// В зависимостях появилась библиотека translator: ^0.1.7
-// Необходимо зайти в pubspec.yaml и нажать на pub get или в корне проекта, в терминале вызвать dart pub get
-// Теперь вы можете создать объект
-// final translator = GoogleTranslator();
-//
-// Используя репозиторий PoemRepository, получите 5 случайных поэм с описанием.
-// Переведите их описания и названия.
-//
-// сделайте это двумя способавми. Через  then и async - await
-
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:translator/translator.dart';
 
-void main() async {
-  forAwait();
-  then();
+void main() {
+  taskWithAwait();
+  taskWithThen();
 }
 
-Future<void> forAwait() async {
+Future<void> taskWithAwait() async {
   final poemRepository = PoemRepository();
   final translator = GoogleTranslator();
   final poemsAwait = await poemRepository.getFiveRandomPoems();
@@ -27,7 +17,7 @@ Future<void> forAwait() async {
   }
 }
 
-Future<void> then() async {
+Future<void> taskWithThen() async {
   final translator = GoogleTranslator();
 
   final poemRepository = PoemRepository();
