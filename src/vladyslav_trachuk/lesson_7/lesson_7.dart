4173c1ae93268f812d81e9a0db9d9409089d1364
Trachukv <vladyslavtrachuk@gmail.com>
2022-09-03 16:47:26 +0200

    Vladyslav Trachuk Lesson 7 (corrected)

--- src/vladyslav_trachuk/lesson_7/lesson_7.dart
+++ src/vladyslav_trachuk/lesson_7/lesson_7.dart
@@ -30,98 +30,93 @@ void main() {
 }
 
 Map<int, List<String>> task1(String text) {
-  final map = <int, List<String>>{};
-  final chekList = List<String>.empty(growable: true);
-  final regExp = RegExp('[а-я]+', caseSensitive: false);
+  final mapOfResult = <int, List<String>>{};
+  final wordList = List<String>.empty(growable: true);
+  final regExp = RegExp('[А-Яа-яa-zA-z]+');
   final Iterable<Match> matches = regExp.allMatches(text);
-  final Set<String> newSet = (matches.map((e) => e.input.substring(e.start, e.end).toLowerCase()).toList()).toSet();
-  for (final String i in newSet) {
-    chekList.clear();
-    final enumerator = RegExp('(^|[А-Яа-яa-z])$i(?![А-Яа-яa-z])', caseSensitive: false);
-    final Iterable<Match> iterations = enumerator.allMatches(text);
-    final iter = iterations.length;
-    final force = map.containsKey(iter);
-    chekList.add(i);
-    if (iter > 1) {
-      if (force == false) {
-        map.putIfAbsent(iter, () => List<String>.from(chekList));
+  final Set<String> setOfCharacter =
+      (matches.map((e) => e.input.substring(e.start, e.end).toLowerCase()).toList()).toSet();
+  for (final String point in setOfCharacter) {
+    wordList.clear();
+    final enumerator = RegExp('(^|[А-Яа-яa-z])$point(?![А-Яа-яA-Za-z])');
+    final Iterable<Match> matches = enumerator.allMatches(text);
+    final keys = matches.length;
+    final force = mapOfResult.containsKey(keys);
+    wordList.add(point);
+    if (keys > 1) {
+      if (!force) {
+        mapOfResult.putIfAbsent(keys, () => List<String>.from(wordList));
       }
-      if (force == true) {
-        map.update(iter, (value) => value + List<String>.from(chekList));
+      if (force) {
+        mapOfResult.update(keys, (value) => value + List<String>.from(wordList));
       }
     }
   }
 
-  final List<Map<int, List<String>>> cases = [];
-  map.forEach((key, value) => cases.add({key: value}));
-  cases.sort((first, twice) {
+  final List<Map<int, List<String>>> dataList = [];
+  mapOfResult.forEach((key, value) => dataList.add({key: value}));
+  dataList.sort((first, twice) {
     final firstly = first.keys.first;
     final twain = twice.keys.first;
     return firstly < twain ? 0 : -1;
   });
-  final Map<int, List<String>> newMap = {for (var element in cases) element.keys.first: element.values.first};
-
-  return newMap;
+  return {for (var element in dataList) element.keys.first: element.values.first};
 }
 
 Map<int, List<String>> task2(String text) {
-  final map = <int, List<String>>{};
-  final chekList = List<String>.empty(growable: true);
-  final regExp = RegExp('[а-я]+', caseSensitive: false);
+  final mapOfResult = <int, List<String>>{};
+  final wordList = List<String>.empty(growable: true);
+  final regExp = RegExp('[А-Яа-яa-zA-z]+');
   final Iterable<Match> matches = regExp.allMatches(text);
-  final Set<String> newSet = (matches.map((e) => e.input.substring(e.start, e.end).toLowerCase()).toList()).toSet();
+  final Set<String> setOfCharacter =
+      (matches.map((e) => e.input.substring(e.start, e.end).toLowerCase()).toList()).toSet();
 
-  for (final String i in newSet) {
-    chekList.clear();
+  for (final String i in setOfCharacter) {
+    wordList.clear();
     final length = i.length;
-    chekList.add(i);
-    final key = map.containsKey(length);
-    if (key == false) {
-      map.putIfAbsent(length, () => List<String>.from(chekList));
+    wordList.add(i);
+    final key = mapOfResult.containsKey(length);
+    if (!key) {
+      mapOfResult.putIfAbsent(length, () => List<String>.from(wordList));
     }
-    if (key == true) {
-      map.update(length, (value) => value + List<String>.from(chekList));
+    if (key) {
+      mapOfResult.update(length, (value) => value + List<String>.from(wordList));
     }
   }
 
-  final List<Map<int, List<String>>> cases = [];
-  map.forEach((key, value) => cases.add({key: value}));
+  final List<Map<int, List<String>>> dataList = [];
+  mapOfResult.forEach((key, value) => dataList.add({key: value}));
 
-  cases.sort((first, twice) {
+  dataList.sort((first, twice) {
     final firstly = first.keys.first;
     final twain = twice.keys.first;
     return firstly < twain ? 1 : -1;
   });
-
-  final Map<int, List<String>> newMap = {for (var element in cases) element.keys.first: element.values.first};
-
-  return newMap;
+  return {for (var element in dataList) element.keys.first: element.values.first};
 }
 
 Map<String, int> task3(String text) {
-  final regExp = RegExp('[а-я]', caseSensitive: false);
-  final map = <String, int>{};
-  final Iterable<Match> chekList = regExp.allMatches(text);
-  final Set<String> newSet = (chekList.map((e) => e.input.substring(e.start, e.end).toUpperCase()).toList()).toSet();
+  final regExp = RegExp('[А-Яа-яa-zA-z]');
+  final mapOfResult = <String, int>{};
+  final Iterable<Match> matches = regExp.allMatches(text);
+  final Set<String> setOfCharacter =
+      (matches.map((e) => e.input.substring(e.start, e.end).toUpperCase()).toList()).toSet();
 
-  for (final String i in newSet) {
-    final iter = RegExp(i, caseSensitive: false);
-    final Iterable<Match> matches = iter.allMatches(text);
-    final k = matches.length;
-    map.putIfAbsent(i, () => k);
+  for (final String item in setOfCharacter) {
+    final keys = RegExp(item, caseSensitive: false);
+    final Iterable<Match> matches = keys.allMatches(text);
+    final key = matches.length;
+    mapOfResult.putIfAbsent(item, () => key);
   }
 
-  final List<Map<String, int>> cases = [];
-  map.forEach((key, value) => cases.add({key: value}));
+  final List<Map<String, int>> dataList = [];
+  mapOfResult.forEach((key, value) => dataList.add({key: value}));
 
-  cases.sort((first, twice) {
+  dataList.sort((first, twice) {
     final firstly = first.values.first;
     final twain = twice.values.first;
     return firstly < twain ? 1 : -1;
   });
 
-  final Map<String, int> newMap = {for (var element in cases) element.keys.first: element.values.first};
-
-  return newMap;
+  return {for (var element in dataList) element.keys.first: element.values.first};
 }
-
