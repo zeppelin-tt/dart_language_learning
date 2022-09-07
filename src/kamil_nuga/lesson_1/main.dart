c60607d705677161a840dc71f41900eb1a101b82
kamilnuga <kamilnogumano143@gmail.com>
2022-09-07 20:16:06 +0500

    refactoring code

--- src/kamil_nuga/lesson_1/main.dart
+++ src/kamil_nuga/lesson_1/main.dart
@@ -1,17 +1,23 @@
+void main() {
+  const int q = 21;
+  const int w = 8;
+  print(simpleDivision(q, w));
+  print(sumOfNumbersForTwoDigit(q));
+  print(simpleRound(8.59)); // Проверка пограничной ситуации в большую сторону
+  print(simpleRound(8.49)); // Проверка пограничной ситуации в меньшую сторону
+  print(sumOfNumbersForThreeDigit(301));
+}
+
 double simpleDivision(int q, int w) => q / w;
 
-int sumOfNumbersForTwoDigit(int n) => n % 10 + n ~/ 10;
+int? sumOfNumbersForTwoDigit(int n) {
+  if (n.toString().length != 2) return null;
+  return n % 10 + n ~/ 10;
+}
 
 int simpleRound(double n) => n.round();
 
-int sumOfNumbersForThreeDigit(int n) => n ~/ 100 + n ~/ 10 % 10 + n % 10;
-
-void main() {
-  const int q = 21;
-  const int  w = 8;
-  print(simpleDivision(q, w));
-  print(sumOfNumbersForTwoDigit(q));
-  print(simpleRound(8.49)); // Проверка пограничной ситуации в большую сторону
-  print(simpleRound(8.49)); // Проверка пограничной ситуации в меньшую сторону
-  print(sumOfNumbersForThreeDigit(301));
+int? sumOfNumbersForThreeDigit(int n) {
+  if (n.toString().length != 3) return null;
+  return n ~/ 100 + n ~/ 10 % 10 + n % 10;
 }
