5f24bd8cab44b94b79c0f36b35ad774763598e39
Alexey Karimurzayev <asxtream@gmail.com>
2022-09-11 01:21:33 +0600

    alexey karimurzayev lesson 10: in process

--- src/alexey_karimurzayev/lesson_10/main.dart
+++ src/alexey_karimurzayev/lesson_10/main.dart
@@ -1,61 +1,78 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
 void main() {
-
-  // stdin.transform(utf8.decoder).transform(const LineSplitter()).listen();
-  takeDataFromConsole();
+  final calculator = Calculator();
+  StreamSubscription<String>? listenToTheConsole;
+  listenToTheConsole = stdin.transform(utf8.decoder).transform(const LineSplitter()).listen((consoleInput) {
+    if (consoleInput == 'exit') {
+      listenToTheConsole?.cancel();
+    } else {
+      calculator.getResult(consoleInput);
+    }
+  });
 }
 
-void takeDataFromConsole() {
-  stdout.write('Введите любое число: ');
-  final double firstNumber = double.parse(stdin.readLineSync()!);
+class Calculator {
+  bool secondOperation = false;
+  late double firstNumber;
+  late double secondNumber;
+  late String operator;
+  late double saveResult;
+  late double result;
 
-  stdout.write('Введите еще одно любое число: ');
-  final double secondNumber = double.parse(stdin.readLineSync()!);
+  void getResult(String consoleInput) {
+    final String inputString = consoleInput.replaceAll(RegExp(r'\s\b|\b\s'), '').replaceAll(',', '.');
+    final List<String> temp = inputString.split(RegExp(r'(?<=\d)(?=[+*/\-%])|(?<=[+*/\-%])(?=\d)')).toList();
 
-  stdout.write('Какую опреацию произвести (выбирете из +, -, /, *): ');
-  final String operator = stdin.readLineSync()!;
+    bool checkInput(List<String> temp) {
+      final checkInputString = temp.join();
+      final symbolCheck = RegExp(r'[^\d+\-/*%.]');
+      if (symbolCheck.hasMatch(checkInputString)) {
+        print('Введены невалидные символы');
+        return false;
+      } else {}
+      if (temp.length > 3) {
+        print('Разрешена только одна операция за раз, или введен лишний оператор');
+        return false;
+      } else {}
+      return true;
+    }
 
-  if (operator == '+') {
-    summarizing(firstNumber, secondNumber);
-  } else if (operator == '-') {
-    subtracting(firstNumber, secondNumber);
-  } else if (operator == '*') {
-    multiply(firstNumber, secondNumber);
-  } else if (operator == '/') {
-    divide(firstNumber, secondNumber);
-  } else if (operator != '+' || operator != '-' || operator != '/' || operator != '*') {
-    print('Ну там же подсказка была... Теперь запускай заново и вводи правильный оператор!');
-    takeDataFromConsole();
+    if (!checkInput(temp)) {
+      return;
+    } else {
+      if (temp.length == 2) {
+        operator = temp[0].split('').first;
+        firstNumber = result;
+        secondNumber = double.parse(temp[1]);
+      } else {
+        firstNumber = double.parse(temp[0]);
+        operator = temp[1].split('').first;
+        secondNumber = double.parse(temp[2]);
+      }
+      if (operator == '+') {
+        result = firstNumber + secondNumber;
+      }
+      if (operator == '-') {
+        result = firstNumber - secondNumber;
+      }
+      if (operator == '*') {
+        result = firstNumber * secondNumber;
+      }
+      if (operator == '/') {
+        if (secondNumber == 0) {
+          print('Делить на ноль нельзя!');
+          return;
+        } else {
+          result = firstNumber / secondNumber; // Есть проблема, ищу решение...
+        }
+      }
+      if (operator == '%') {
+        result = firstNumber % secondNumber;
+      }
+    }
+    print(result);
   }
-
-
-
-
 }
-
-void summarizing(double firstNumber, double secondNumber) {
-  final double result = firstNumber + secondNumber;
-
-  print('$firstNumber + $secondNumber = $result');
-}
-
-void subtracting(double firstNumber, double secondNumber) {
-  final double result = firstNumber - secondNumber;
-
-  print('$firstNumber - $secondNumber = $result');
-}
-
-void multiply(double firstNumber, double secondNumber) {
-  final double result = firstNumber * secondNumber;
-
-  print('$firstNumber * $secondNumber = $result');
-}
-
-void divide(double firstNumber, double secondNumber) {
-  final double result = firstNumber / secondNumber;
-
-  print('$firstNumber / $secondNumber = $result');
-}
-
